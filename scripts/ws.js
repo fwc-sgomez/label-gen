3593d347e48b3e4263eb0430abd939c81029e65d
fwc-sgomez <s.gomez@fibreworkscomposites.com>
2025-12-03 10:53:54 -0500

    ws fixes
    ws fixes, but its still very much broken...

--- scripts/ws.js
+++ scripts/ws.js
@@ -1,23 +1,22 @@
 const wsUri = "ws://127.0.0.1/";
 let ws;
-let wsImageData
-
-ws.onopen = (e) => {
-  ws.send(wsImageData)
-}
-
-ws.onmessage = (e) => {
-  console.log(`response: ${e.data}`);
-}
-
-ws.onerror = (e) => {
-  alert(`server error: ${e.data}`);
-}
+let wsImageData;
 
 function wsSetImage(imageData) {
-    wsImageData = imageData
+    wsImageData = imageData;
 }
 
 function wsStart(){
     ws = new WebSocket(wsUri);
+    ws.onopen = (e) => {
+        ws.send(wsImageData)
+    }
+    
+    ws.onmessage = (e) => {
+        console.log(`response: ${e.data}`);
+    }
+    
+    ws.onerror = (e) => {
+        showWarningMessage(`server error: ${e.data}`);
+    }
 }
