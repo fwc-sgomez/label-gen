e1e80692073e4c78759c21ca2e27d4885a75b22b
fwc-sgomez <s.gomez@fibreworkscomposites.com>
2025-12-13 15:20:29 -0500

    Update ws.js
    ws multiple attempts rather than giving up at first one

--- scripts/ws.js
+++ scripts/ws.js
@@ -1,15 +1,17 @@
 const wsUri = "ws://127.0.0.1:21845/";
 let ws;
 let wsImageData;
+let wsStartAttemps = 0;
 
 function wsSetImage(imageData) {
     wsImageData = imageData;
 }
 
 function wsStart(){
+    wsStartAttemps++
     ws = new WebSocket(wsUri);
     ws.onopen = (e) => {
-        ws.send(wsImageData)
+        ws.send(wsImageData);
     }
     
     ws.onmessage = (e) => {
@@ -17,6 +19,12 @@ function wsStart(){
     }
     
     ws.onerror = (e) => {
-        showWarningMessage(`server error: ${e.data}`);
+        showWarningMessage(`error connecting to FWCPrintApp: ${e.data}\nattempt ${wsStartAttemps} of 3`);
+        if (wsStartAttemps < 3){
+            wsStart()
+        } else {
+            showWarningMessage('Unable to connect to the print app.')
+            wsStartAttemps = 0
+        }
     }
 }
